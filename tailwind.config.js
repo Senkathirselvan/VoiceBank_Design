73f07f74278b254b18dc2535b0b824f2ed56b1f8
Senkathirselvan <125534240+Senkathirselvan@users.noreply.github.com>
2024-03-31 15:40:32 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        'GreenishBlue':'#388087',
+        'Neptune':'#6FB3B8',
+        'SpringWood':'#F6F6F2',
+        'PowderBlue':'#BADFE7',
+        'SurfCrest':'#C2EDCE'
+      
+      },
+      icon:{
+        'colors':'pink'
+      }
+    },
+  },
+  plugins: [],
+}
+
